c76891a3d914dbea11eb5220257e93581dc96c37
vasylevskaya <vasylevska98@gmail.com>
2024-10-16 00:31:02 +0200

    Fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage ';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
