1cbfc4f80e5ce9c85bb8f760418ec1b451bc0a0e
vasylevskaya <vasylevska98@gmail.com>
2024-11-27 00:37:07 +0100

    fixes app

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         <Routes>
           <Route path={ROUTES.product_page} element={<ProductPage />} />
           <Route path={ROUTES.privacy_policy_page} element={<PrivacyPolicyPage />} />
-          <Route path={ROUTES.homgit e_page} element={<HomePage />} />
+          <Route path={ROUTES.home_page} element={<HomePage />} />
           <Route path={'*'} element={<HomePage />} />
         </Routes>
         <ContactForm />
