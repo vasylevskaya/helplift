c7408171a347680fdbcd04a16566b76ff95fa85f
vasylevskaya <vasylevska98@gmail.com>
2024-11-27 00:35:57 +0100

    removed 404 script and adds hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
@@ -23,7 +23,7 @@ const App = () => {
         <Routes>
           <Route path={ROUTES.product_page} element={<ProductPage />} />
           <Route path={ROUTES.privacy_policy_page} element={<PrivacyPolicyPage />} />
-          <Route path={ROUTES.home_page} element={<HomePage />} />
+          <Route path={ROUTES.homgit e_page} element={<HomePage />} />
           <Route path={'*'} element={<HomePage />} />
         </Routes>
         <ContactForm />
