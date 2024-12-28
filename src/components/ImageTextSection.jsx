d4f2ef94b66ec48d39fd7fcec76ac7d0e7f92616
vasylevskaya <vasylevska98@gmail.com>
2024-12-29 00:25:34 +0100

    Remove lazy load for background img

--- src/components/ImageTextSection.jsx
+++ src/components/ImageTextSection.jsx
@@ -1,29 +1,13 @@
-import React, { Suspense } from 'react';
-import Loader from './Loader';
-
-const LazyImage = React.lazy(() =>
-  new Promise((resolve) => {
-    const image = new Image();
-    image.src = require('../assets/images/scene.png'); // Load your image dynamically
-    image.onload = () =>
-      resolve({
-        default: () => (
-          <Suspense fallback={<Loader />}>
-            <img
-              src={image.src}
-              className='section-image-text_bg-img'
-              alt='Background'
-            />
-          </Suspense>
-        ),
-      });
-  })
-);
+import React from 'react';
+import bgImg from '../assets/images/scene.png';
 
 const ImageTextSection = () => {
   return (
     <div className='section-image-text'>
-      <LazyImage />
+      <img
+        src={bgImg}
+        className='section-image-text_bg-img'
+      />
       <div className='section-image-text_content'>
         <p className='section-image-text_content_title'>
           Виробництво підйомників
