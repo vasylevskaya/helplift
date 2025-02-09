5302bb191ad9681fcbbd68f89dce9b20fd5ad58e
vasylevskaya <vasylevska98@gmail.com>
2025-02-09 19:52:16 +0100

    Change phone number

--- src/components/ContactData.jsx
+++ src/components/ContactData.jsx
@@ -3,7 +3,7 @@ const ContactData = () => {
     <div className='contact-data'>
       <p>
         <i className="fas fa-phone"></i>
-        <span className="font-grotesque">+38 (099) 678 67 89</span>
+        <span className="font-grotesque">+380 (50) 785 79 96</span>
       </p>
       <p>
         <i className="fas fa-envelope"></i> helplift@gmail.com
